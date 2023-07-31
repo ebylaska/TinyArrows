1c7fd7a6a1f0c0747714932f0253d8c6a9d7c11f
Eric Bylaska <bylaska@gmail.com>
2023-07-30 21:14:49 -0700

    ...EJB

--- init/init-db.sh
+++ init/init-db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-gunzip /docker-entrypoint-initdb.d/backup/eric3.sql.gz
+if [ ! -f /docker-entrypoint-initdb.d/backup/eric3.sql ]; then
+    gunzip /docker-entrypoint-initdb.d/backup/eric3.sql.gz
+fi
 
 DB_EXISTS=$(mariadb -uarrows -preaction -e "SHOW DATABASES LIKE 'TNT_Project';" | grep "TNT_Project" > /dev/null; echo "$?")
 
